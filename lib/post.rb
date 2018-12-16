36e6a0ccee93734c5597e672def1ac41589fd166
Duane Felton <dfelton2010@gmail.com>
2018-12-16 17:46:57 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,16 @@
 class Post
   
-  attr_accessor :name
+  attr_accessor :name, :title
   
   @@all = []
   
   def initialize(title)
     @title = title
     @@all << self
+  end
+  
+  def title
+    @title
   end
   
   def self.all

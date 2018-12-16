99adfa1dc3712bb7f1ecb3a74f52d514c6566fa5
Duane Felton <dfelton2010@gmail.com>
2018-12-16 17:22:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,6 @@ class Song
     @@all << self
   end
   
-<<<<<<< HEAD
   def self.all
     @@all
   end
@@ -22,15 +21,5 @@ class Song
     end
   end
   
-=======
-  def all
-    @@all.inspect
-  end
-  
-  def artist_name
-    self.artist.name
-  else nil
-  end
->>>>>>> b5d357c32827e0f453ce306ddb34122b44962eb5
 end
     
